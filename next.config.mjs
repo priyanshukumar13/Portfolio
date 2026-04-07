74622dfe70980624ef0ca6bdd58f71aaab21fad5
Priyanshu kumar <pk2525507@gmail.com>
2026-04-08 03:05:04 +0530

    fix: add typescript dependency and resolve FloatingSocials hook warningssk

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["picsum.photos"], // Add the hostname here
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ["picsum.photos"], // Add the hostname here
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
